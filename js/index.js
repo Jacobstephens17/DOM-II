e3e80d638c613c826caeb88b5ab7db0a134b5d67
Jacob Stephens <jacobstephens2@gmail.com>
2020-12-01 16:33:53 -0800

    added a bunch of event listeners, still need some more

--- js/index.js
+++ js/index.js
@@ -1,7 +1,42 @@
 const navButtons = document.querySelectorAll('a');
-
 navButtons.forEach((elem)=>{
     elem.addEventListener('mouseover', function(){
         elem.style.backgroundColor = "red"
+        
     })
+})
+navButtons.forEach((elem) =>{
+    elem.addEventListener('dblclick', function(){
+        elem.style.backgroundColor = "green"
+    })
+})
+
+const pars = document.querySelectorAll('.btn');
+pars.forEach((elem) =>{
+    elem.addEventListener('load', function(){
+            elem.style.backgroundColor = "purple"
+    })
+})
+
+
+
+const mainHead = document.querySelector('h2');
+
+document.addEventListener('keydown', function(){
+    mainHead.style.fontSize = '10rem';
+    mainHead.style.color = 'purple';
+    
+})
+
+
+const pics = document.querySelector('img');
+
+document.addEventListener('scroll', function(){
+    pics.style.display = 'none';
+})
+
+const pageBackgr = document.querySelector('body');
+
+window.addEventListener('wheel', function(){
+    pageBackgr.style.backgroundColor = 'black';
 })
